7de4c11b41c784ade3fc70af7f9d211cefbb30c3
Teng Xuan Tan <tantx.sg@gmail.com>
2019-11-15 13:00:53 +0900

    初版作成

---
+++ ProjectClapArt/Assets/SelectManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] ImageSets;
+    int selection = 0;
+    Vector2 curPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curPos = (Vector2)Input.mousePosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mov = (Vector2)Input.mousePosition - curPos;
+        if (mov.x > 10.0f)
+        {
+            selection = 2;
+            ChangeImage();
+        }
+        else if (mov.x < -10.0f)
+        {
+            selection = 1;
+            ChangeImage();
+        }
+        curPos = Input.mousePosition;
+    }
+
+    void ChangeImage ()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            ImageSets[i].SetActive(i == selection);
+        }
+    }
+}
