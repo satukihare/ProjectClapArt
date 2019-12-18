d303650a0b2eef3f144f5632efb418d75283f108
Sato_Hiroto <sato.hiroto0811@gmail.com>
2019-12-19 07:39:16 +0900

    モデル差し替え

---
+++ ProjectClapArt/Assets/Result/ResultManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ResultManager : MonoBehaviour
+{
+
+    [SerializeField]
+    PlayableDirector Director;
+
+    bool ScoreVoice = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Director.state == PlayState.Paused)
+        {
+            if (!ScoreVoice)
+            {
+                ScoreVoice = true;
+
+
+
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Director.state != PlayState.Paused)
+            {
+                Director.time = Director.duration;
+            }
+            else
+            {
+                Transition.instance.ChangeScene("TitleScene");
+            }
+        }
+    }
+}
