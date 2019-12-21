1db9345daf79652887b4f4f6db27a196df0603fe
Sato_Hiroto <sato.hiroto0811@gmail.com>
2019-12-22 01:45:26 +0900

    Titleにチーム名とか作品名のヤツ追加

---
+++ ProjectClapArt/Assets/TitleAnimeScripts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnimeScripts : MonoBehaviour
+{
+    [SerializeField]private AudioClip GroupLogo_Announce;
+    
+    [SerializeField] private AudioClip Title_Announce;
+
+    AudioSource audioSource;
+    [SerializeField] private AudioSource TitleBGM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void GroupLogoAnnounce()
+    {
+        audioSource.PlayOneShot(GroupLogo_Announce);
+
+
+        TitleBGM.PlayDelayed(1.5f);
+        TitleBGM.Play();
+
+        audioSource.clip = Title_Announce;
+        audioSource.PlayDelayed(2.5f);
+        audioSource.Play();
+
+ 
+    }
+
+   
+}
