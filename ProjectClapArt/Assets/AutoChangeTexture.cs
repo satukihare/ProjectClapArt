b679cdc7b9bc57f54eda9c756a79894a930edfcd
Sato_Hiroto <sato.hiroto0811@gmail.com>
2020-01-21 08:25:44 +0900

    Resultでのキャラクターの切り替えの実装 ProjectがBackground状態でも進むように変更してゲームを止まらないように

---
+++ ProjectClapArt/Assets/AutoChangeTexture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class AutoChangeTexture : MonoBehaviour
+{
+   // [SerializeField]
+    RawImage CharImage;
+
+    [SerializeField]
+    Texture Nagi, Kai;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharImage = GetComponent<RawImage>();
+
+       
+        if (SelectData.chara_select == 0)
+        {
+            CharImage.texture = Nagi;
+        }
+        else
+        {
+            CharImage.texture = Kai;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
